3834e56eea98d751fc6532c8d34f276cd732cc4e
Nick Walter <nick@zappycode.com>
2016-08-12 16:01:10 -0600

    Fixed error so that snaps show up from the correct user

--- Snapchat/SelectUserViewController.swift
+++ Snapchat/SelectUserViewController.swift
@@ -55,7 +55,7 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let user = users[indexPath.row]
         
-        let snap = ["from":user.email, "description":descrip, "imageURL":imageURL, "uuid":uuid]
+        let snap = ["from":FIRAuth.auth()!.currentUser!.email!, "description":descrip, "imageURL":imageURL, "uuid":uuid]
         
         FIRDatabase.database().reference().child("users").child(user.uid).child("snaps").childByAutoId().setValue(snap)
         
